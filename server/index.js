e32e736dfdfc7a394bf4092896ceb5d5ddc1b03a
Khushi Verma <mm0981389@gmail.com>
2026-06-05 16:32:08 +0530

    Day 1 completed

---
+++ server/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
